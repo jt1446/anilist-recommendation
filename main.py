2cb0be78cdf0850f3bf3073f8eb20fd9b818d17a
ms358 <melvinshajee358@gmail.com>
2026-03-11 16:43:53 -0400

    basic main script

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("this is the main.py!")
+
+from modules.gnn_model import GNNEncoder
+from modules.seq_model import RNNPredictor
+from modules.trainer import train
+
+# 1. Load Data
+data = load_processed_data() 
+# 2. Initialize Models
+gnn = GNNEncoder()
+rnn = RNNPredictor()
+# 3. Train
+train(gnn, rnn, data)
